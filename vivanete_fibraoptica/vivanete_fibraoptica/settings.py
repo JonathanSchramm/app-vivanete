f1de88f00476950c0e09815d9eac876f374803a7
seu nome de usuário <jonathan.biomed2@gmail.com>
2024-09-20 14:50:35 -0300

    debug off

--- vivanete_fibraoptica/vivanete_fibraoptica/settings.py
+++ vivanete_fibraoptica/vivanete_fibraoptica/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jmkw@0jss_pt4ue51vcen3=mji_x722$a8mjmukcsbt94v+k3$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
